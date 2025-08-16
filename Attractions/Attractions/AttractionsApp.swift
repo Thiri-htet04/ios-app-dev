3eccb66d1b4cbf0a8c1c5be76ad5e2d7ad7fc9be
Thiri Htet <thirihtet7864@gmail.com>
2025-08-16 09:46:50 +0700

    adding Attractions classwork

---
+++ Attractions/Attractions/AttractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttractionsApp.swift
+//  Attractions
+//
+//  Created by Thiri Htet on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AttractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
