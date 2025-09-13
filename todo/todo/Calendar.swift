803461108d15cf37f16cbb7986e0fb5ee62fcbeb
Thiri Htet <thirihtet7864@gmail.com>
2025-09-13 11:52:12 +0700

    modified calendar start day

--- todo/todo/Calendar.swift
+++ todo/todo/Calendar.swift
@@ -9,26 +9,47 @@ import SwiftUI
 
 struct Calendar: View {
     
+    let startAt : Int = 2 //Monday = 0, Tuesday = 1, ... , Sunday = 6
+    
+    
     @State var dates: [Int] = []
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
+    let weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+    
     var body: some View {
         ScrollView{
+            HStack{
+                ForEach(weekdays, id: \.self){weekday in
+                    Text(weekday)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        .frame(maxWidth: .infinity)
+                }
+            }
             LazyVGrid(columns: columns, spacing: 20){
                 ForEach(dates, id: \.self){date in
-                    Text("\(date)")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .frame(width: 40, height: 60)
-                        .background(.pink)
-                        .foregroundStyle(.white)
-                        .clipShape(.rect(cornerRadius: 10))
+                    
+                    if date > 0 {
+                        Text("\(date)")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .frame(width: 40, height: 60)
+                            .background(.pink)
+                            .foregroundStyle(.white)
+                            .clipShape(.rect(cornerRadius: 10))
+                    }else{
+                        Text("")
+                    }
+                    
                     
                 }
             }
         }
         .onAppear {
-            for i in 1...31 {
+            var initial = 1
+            initial -= startAt
+            for i in initial...31 {
                 dates.append(i)
             }
         }
