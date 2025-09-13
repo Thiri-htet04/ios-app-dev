f9a8e5034694809304074c0bcb9fd97d082f9e64
Thiri Htet <thirihtet7864@gmail.com>
2025-09-13 09:19:05 +0700

    modified job-hunter and add initial commit to todo

---
+++ todo/todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Thiri Htet on 13/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
